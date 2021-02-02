554dcd1fa0d57908f229a79005ce4a8320df0c12
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-02-01 23:24:44 -0300

    encapsulamento

--- ExemplosOOP/src/InstanciaPessoa.java
+++ ExemplosOOP/src/InstanciaPessoa.java
@@ -2,13 +2,14 @@ public class InstanciaPessoa {
 
     public static void main(String[] args){
 
-        Pessoa pessoinha = new Pessoa("Vandeilson");
+        Pessoa pessoinha = new Pessoa("Vandeilson",29, 8, 1995);
 
         System.out.println(pessoinha.getNome());
+        System.out.println(pessoinha.getDataNascimento());
+        System.out.println(pessoinha.calculaIdade());
 
-        pessoinha.setNome(("Lima Nobre"));
-
-        System.out.println((pessoinha.getNome()));
+        pessoinha.setNome("Van Der Wilson");
+        System.out.println(pessoinha.getNome());
 
     }
 }
--- ExemplosOOP/src/Pessoa.java
+++ ExemplosOOP/src/Pessoa.java
@@ -1,13 +1,26 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+
 public class Pessoa {
+
+    private String nome;
+    private LocalDate dataNascimento;
 
     public Pessoa(){
 
     }
 
-    public Pessoa(String nome) {this.nome = nome;}
+    public Pessoa(String nome, int dia, int mes, int ano) {
+        this.nome = nome;
+        this.dataNascimento = LocalDate.of(ano, mes, dia);
+    }
 
-    private String nome;
     public String getNome() {return nome;}
 
     public void setNome(String nome) {this.nome = nome;}
+
+    public LocalDate getDataNascimento() {return dataNascimento;}
+
+    public int calculaIdade() {return Period.between(dataNascimento, LocalDate.now()).getYears();}
 }
