874821e48338b3d9d0fba9d91eea1699206c0b21
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-02-08 20:53:35 -0300

    composições

---
+++ desenvolvimento_avancado/src/ParadigmaFuncional/ComposicaoFuncoes.java
@@ -0,0 +1,13 @@
+package ParadigmaFuncional;
+
+import java.util.Arrays;
+
+public class ComposicaoFuncoes {
+    public static void main(String[] args){
+        int[] valores = {1, 2, 3, 4};
+        Arrays.stream(valores)
+                .filter(numero -> numero % 2 == 0)
+                .map(numero -> numero *2)
+                .forEach(numero -> System.out.println(numero));
+    }
+}
