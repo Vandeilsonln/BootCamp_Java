0f1171c239514c11899bb8df94ec0ac4a5453d63
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-02-15 22:24:50 -0300

    update

--- desenvolvimento_avancado/src/InterfacesFuncionais/FuncaoAltaOrdem.java
+++ desenvolvimento_avancado/src/InterfacesFuncionais/FuncaoAltaOrdem.java
@@ -3,7 +3,14 @@ package InterfacesFuncionais;
 public class FuncaoAltaOrdem {
     public static void main(String[] args){
         Calculo soma = (a,b) -> a+b;
+        Calculo subtracao = (a,b) -> a-b;
+        Calculo divisao = (a,b) -> a/b;
+        Calculo multiplacao = (a,b) -> a*b;
+
         System.out.println(executarOperacao(soma,1,3));
+        System.out.println(executarOperacao(subtracao,5,3));
+        System.out.println(executarOperacao(divisao,9,3));
+        System.out.println(executarOperacao(multiplacao,2,3));
     }
 
     public static int executarOperacao(Calculo calculo, int a, int b){
@@ -11,6 +18,7 @@ public class FuncaoAltaOrdem {
     }
 }
 
+@FunctionalInterface
 interface Calculo{
     public int calcular(int a, int b);
 }
