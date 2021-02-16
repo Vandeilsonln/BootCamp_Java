745b32c0a2b92ae70c469f3a02a9677d779f3609
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-02-15 22:43:46 -0300

    consumer

---
+++ desenvolvimento_avancado/src/InterfacesFuncionais/Consumidores.java
@@ -0,0 +1,13 @@
+package InterfacesFuncionais;
+
+import java.util.function.Consumer;
+
+public class Consumidores {
+    public static void main(String[] args){
+
+
+        Consumer<String> imprimirUmaFrase = System.out::println;
+        Consumer<String> imprimirUmaFrase2 = frase -> System.out.println(frase);
+        imprimirUmaFrase.accept("Hello World");
+    }
+}
