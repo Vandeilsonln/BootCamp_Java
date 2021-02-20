bb5ea39258d83a79f5309b680a6153f6eed5b0d9
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-02-20 00:25:58 -0300

    initial commit

---
+++ BD_JPA/src/main/java/com/digitalinnovationone/jpa_basico/JpaBasicoApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.jpa_basico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBasicoApplication.class, args);
+	}
+
+}
---
+++ BD_JPA/src/test/java/com/digitalinnovationone/jpa_basico/JpaBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.jpa_basico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
